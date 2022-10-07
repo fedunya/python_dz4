6a77ca9e97f6953f5f7a104690bf8a97c5f7b9a3
Fedor <farlep1970@yandex.ru>
2022-10-07 21:05:29 +0300

    task_17

---
+++ task_17.py
@@ -0,0 +1,24 @@
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+
+def user_input(a):
+    num = input(a)
+    while not num.isdigit() or num == '0':
+        print('Неверный ввод, повторите!')
+        num = input(a)
+    num = int(num)
+    return num
+
+import os
+os.system("cls")
+num = user_input('Введите натуральное число: ')
+factors = []
+d = 2
+while d * d <= num:
+        if num % d == 0:
+            factors.append(d)
+            num//=d
+        else:
+            d += 1
+factors.append(num)
+print(factors)
